734497bbcf78c1c0d7b36a2da681eec4afa0edc9
ilsa23 <ilze.sarai@gmail.com>
2015-04-11 12:06:50 -0500

    asdf

--- js/actions.js
+++ js/actions.js
@@ -8,12 +8,23 @@ var fn = {
     },
     init: function(){
         $('#acelerometro ul.individual li').tap(fn.acTap);
+        $('#brujula ul.individual li').tap(fn.brTap);
     },
     acTap: function(){
         if($(this).index()==0)
             ac.iniciar();//Iniciar
         else
             ac.detener();//Detener
+    },
+    brTap: function(){
+        switch($(this).index()){
+        case 0:
+                br.iniciar();
+                break;
+        case 1:
+                br.detener();
+                break;
+        }
     }
 };
 $(fn.ready);
