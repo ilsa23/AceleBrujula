3908791be08bb60376ebffee58d960b9af33ec88
ilsa23 <ilze.sarai@gmail.com>
2015-04-11 10:51:57 -0500

    cvb

---
+++ js/acelerometro.js
@@ -0,0 +1,29 @@
+var ac =
+    { watchID: null,
+     satisfactorio: function(acc)
+     {$('#acelerometro h2').html('X: ' + acc.x + '<br>' +
+                                 'Y: ' + acc.y + '<br>' +
+                                 'Z: ' + acc.z);    
+     },
+    error: function(err)
+    {
+        alert('Acelerómetro Error: '+err.code)
+    },
+    opc: {frecuency: 500},
+    iniciar: function()
+    {
+        if(ac.watchID == null)
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.satisfactorio,ac.error,ac.opc); 
+    },
+    detener: function()
+    {
+        if(ac.watchID)
+        {
+            navigator.accelerometer.clearwatch(ac.watchID);
+            ac.watchID = null;
+            $('#acelerometro h2').html('Detenido');
+            
+        }
+    }
+    
+    };
---
+++ js/actions.js
@@ -0,0 +1,21 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+var fn = 
+    {   ready: function()
+     {  document.addEventListener('deviceready',fn.init,false);
+     },
+        init: function()
+     { $('#acelerometro ul.individual li').tap(fn.acTap);
+     },
+    acTap: function()
+    {   if($(this).index==0)
+            ac.iniciar();
+        else
+            //detener
+    }
+    };
+
+$(fn.ready);
