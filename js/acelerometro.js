90aa2dc0f5827ee5d6e102fb6b0ffc5b575a9d14
ilsa23 <ilze.sarai@gmail.com>
2015-04-11 11:16:39 -0500

    sadf

--- js/acelerometro.js
+++ js/acelerometro.js
@@ -1,29 +1,23 @@
-var ac =
-    { watchID: null,
-     satisfactorio: function(acc)
-     {$('#acelerometro h2').html('X: ' + acc.x + '<br>' +
-                                 'Y: ' + acc.y + '<br>' +
-                                 'Z: ' + acc.z);    
-     },
-    error: function(err)
-    {
-        alert('Acelerómetro Error: '+err.code)
+var ac = {
+    watchID: null,
+    satisfactorio: function(acc){
+        $('#acelerometro h2').html('X: ' + acc.x + '<br>' +
+                                   'Y: ' + acc.y + '<br>' +
+                                   'Z: ' + acc.z);
     },
-    opc: {frecuency: 500},
-    iniciar: function()
-    {
+    error: function(err){
+        alert('Acelerómetro Error: '+err.code);
+    },
+    opc: { frequency: 500 },
+    iniciar: function(){
         if(ac.watchID == null)
-            ac.watchID = navigator.accelerometer.watchAcceleration(ac.satisfactorio,ac.error,ac.opc); 
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.satisfactorio,ac.error,ac.opc);
     },
-    detener: function()
-    {
-        if(ac.watchID)
-        {
-            navigator.accelerometer.clearwatch(ac.watchID);
+    detener: function(){
+        if(ac.watchID){
+            navigator.acceletometer.clearWatch(ac.watchID);
             ac.watchID = null;
             $('#acelerometro h2').html('Detenido');
-            
         }
     }
-    
-    };
+};
--- js/actions.js
+++ js/actions.js
@@ -1,21 +1,19 @@
-//actions
 var jQT = new $.jQTouch({
 	themeSelectionSelector: '#jqt'
 });
 
-var fn = 
-    {   ready: function()
-     {  document.addEventListener('deviceready',fn.init,false);
-     },
-        init: function()
-     { $('#acelerometro ul.individual li').tap(fn.acTap);
-     },
-    acTap: function()
-    {   if($(this).index==0)
-            ac.iniciar();
+var fn = {
+    ready: function(){
+        document.addEventListener('deviceready',fn.init,false);
+    },
+    init: function(){
+        $('#acelerometro ul.individual li').tap(fn.acTap);
+    },
+    acTap: function(){
+        if($(this).index()==0)
+            ac.iniciar();//Iniciar
         else
-            //detener
+            ac.detener();//Detener
     }
-    };
-
+};
 $(fn.ready);
